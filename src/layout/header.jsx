04a6ab32917be67e5fcf68dc0b36d3e2459566ff
GHost7751 <98162320+GHost7751@users.noreply.github.com>
2022-01-31 19:01:51 +0300

    react-movies
    react-movies

---
+++ src/layout/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return  <nav className="blue lighten-3">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">React Movis</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+      </ul>
+    </div>
+  </nav>
+}
+
+export {Header}
